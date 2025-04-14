bc40c4f8f17c4722b3e3d974d28b18b08bd77afd
morde <mordechai.hadad01@gmail.com>
2025-04-14 18:45:27 +0300

    changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
+	import ContextMenu from '$lib/ContextMenu.svelte';
+	import { fly } from 'svelte/transition';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
+
+<ContextMenu class="border border-neutral-500 bg-neutral-100 shadow-lg" inTransition={fly} />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import ContextMenu from '$lib/ContextMenu.svelte';
 	import { openContextMenu } from '$lib/functions.js';
-	import { fly } from 'svelte/transition';
 </script>
 
-<ContextMenu class="border border-neutral-500 bg-neutral-100 shadow-lg" inTransition={fly} />
 
 <div class="flex w-full justify-end">
 	<button
